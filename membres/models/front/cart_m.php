d78f5f60ea18a7739ee0ae0fa13d71bd72a6db9b
cedtfe <ced.tfe@gmail.com>
2017-10-05 22:20:03 +0200

    [html2pdf]
    html2pdf + librairie
    htaccess

---
+++ membres/models/front/cart_m.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//https://www.codeigniter.com/userguide2/database/index.html
+class Cart_m extends CI_Model
+{
+
+
+  public function __construct()
+  {
+        // Call the Model constructor
+        parent::__construct();
+  }
