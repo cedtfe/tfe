8de01a08c866e75e82c33a468110252f57431e40
MatthieuDelitoon <matthieu@delitoon.com>
2017-10-05 21:08:41 +0200

    [themes]
    modification js

--- membres/themes/default.php
+++ membres/themes/default.php
@@ -7,15 +7,14 @@
 
       <link type="text/css" rel="stylesheet" href="./assets/front/css/materialize.css"  media="screen,projection"/>
       <link type="text/css" rel="stylesheet" href="./assets/front/css/basic.css"  media="screen,projection"/>
-
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="./assets/front/js/materialize.min.js"></script>
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
     <body>
       <?php echo $output; ?>
-      <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-      <script type="text/javascript" src="./assets/front/js/materialize.min.js"></script>
+      
     </body>
   </html>
